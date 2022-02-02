40153103e067e022acaf1f923decdc23f1f56483
davidvujic <david@vujic.se>
2022-02-02 23:40:33 +0100

    fix: organize package includes in a root namespace, fixing the broken 'package includes' packaging.

--- poetry_multiproject_plugin/overrides/builders.py
+++ poetry_multiproject_plugin/overrides/builders.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Optional
 
 from poetry.core.masonry.builders.builder import BuildIncludeFile
+from poetry_multiproject_plugin.workspace import namespacing
 
 
 def relative_to_workspace(target) -> Path:
@@ -20,7 +21,7 @@ def relative_to_project_root(self) -> Path:
 
 def relative_to_source_root(self) -> Path:
     if self.workspace:
-        return relative_to_workspace(self)
+        return namespacing.create_namespaced_path(self.path, self.workspace)
 
     if self.source_root is not None:
         return self.path.relative_to(self.source_root)
